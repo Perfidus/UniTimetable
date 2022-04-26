a480bbedc688c161cb515af87e79dce9e1388f53
nkukava <nikolozkukava@192.168.1.9>
2022-04-26 03:05:58 +0300

    adding files

---
+++ cal.py
@@ -0,0 +1,103 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+import pickle
+import os.path
+import datetime
+import locale
+
+locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")  # Setting locale to Russian
+
+# Importing data
+filename = 'data.pkl'
+with open(filename, 'rb') as f:
+    imported_data = pickle.load(f)
+
+# Authorization
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+creds = None
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pkl'):
+    creds = pickle.load(open("token.pkl", "rb"))
+    service = build("calendar", "v3", credentials=creds)
+else:
+    flow = InstalledAppFlow.from_client_secrets_file("client_secret.json",
+                                                     scopes=SCOPES)
+    creds = flow.run_local_server(port=0)
+    pickle.dump(creds, open("token.pkl", "wb"))
+    creds = pickle.load(open("token.pkl", "rb"))
+    service = build("calendar", "v3", credentials=creds)
+
+
+def create_event(start_time_format, end_time_format, summary, description,
+                 location):
+    event = {
+        'summary': summary,
+        'location': location,
+        'description': description,
+        'start': {
+            'dateTime': start_time_format,
+            'timeZone': 'Europe/Moscow',
+        },
+        'end': {
+            'dateTime': end_time_format,
+            'timeZone': 'Europe/Moscow',
+        },
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'popup', 'minutes': 60},
+            ],
+        },
+    }
+    return service.events().insert(
+        calendarId='tot3a4vhmmo3jup9rbko7aqqas@group.calendar.google''.com',
+        body=event).execute()
+
+
+# Creating events in Google Calendar
+lesson_day = 0
+while lesson_day < len(imported_data):
+    lesson_id = 1  # Not 0 to avoid copying a date
+    if imported_data[lesson_day][0] != "None":
+        while lesson_id <= len(imported_data[lesson_day]) - 1:
+            print(lesson_day, lesson_id)
+            d = imported_data[lesson_day][
+                0]  # Retrieving date and lesson start time and formatting it
+            t_start = imported_data[lesson_day][lesson_id][0][0:2] + " " + \
+                      imported_data[lesson_day][lesson_id][0][3:5]
+            t_end = imported_data[lesson_day][lesson_id][0][6:8] + " " + \
+                    imported_data[lesson_day][lesson_id][0][9:]
+            start_time = datetime.datetime.strptime(d + " " + t_start,
+                                                    "%d-%b. %H %M")
+            start_time = start_time.replace(year=2022)
+            start_time_ready = start_time.strftime("%Y-%m-%dT%H:%M:%S")
+            end_time = datetime.datetime.strptime(d + " " + t_end,
+                                                  "%d-%b. %H %M")
+            end_time = end_time.replace(year=2022)
+            end_time_ready = end_time.strftime("%Y-%m-%dT%H:%M:%S")
+
+            lesson_name = imported_data[lesson_day][lesson_id][
+                1]  # Retrieving lesson name
+
+            lesson_description = imported_data[lesson_day][lesson_id][
+                2]  # Retrieving professor name
+            if lesson_name == "Физическая культура и спорт ":
+                lesson_location = "Волейбольный зал"
+            elif lesson_name == "Business English":
+                lesson_location = "Дистанционно"
+            elif lesson_name == "Испанский язык":
+                lesson_location = "Дистанционно"
+            else:
+                lesson_location = imported_data[lesson_day][lesson_id][3]  #
+                # Retrieving lesson_location
+            create_event(start_time_ready, end_time_ready, lesson_name,
+                         lesson_description,
+                         lesson_location)  # Creating event in the calendar
+            lesson_id += 1
+        else:
+            lesson_day += 1
+    else:
+        lesson_day += 1
---
+++ gspread.py
@@ -0,0 +1,278 @@
+import gspread
+from google.oauth2 import service_account
+import regex as re
+import pickle
+
+""" Setting up an API and authorizing """
+scope = ["https://spreadsheets.google.com/feeds",
+         'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
+credentials = service_account.Credentials.from_service_account_file(
+    "credentials.json")
+scoped_credentials = credentials.with_scopes(scope)
+client = gspread.authorize(scoped_credentials)
+
+""" Setting up a worksheet """
+sheet = client.open_by_url(
+    'https://docs.google.com/spreadsheets/d'
+    '/1pNHiTmrgEBjON2kh_KEzIsDIqPTKIkaQ6H0lr29lhYM/edit#gid=1647235932')  #
+# Opening a spreadsheet by url
+worksheetName = '3 курс Менеджмент'
+worksheet = sheet.worksheet(
+    worksheetName)  # Selecting a worksheet by its name
+
+""" Main part of the code """
+regex_numbers = re.compile(
+    '([0-9][0-9]-апр.|[0-9]-апр.)')  # Creating a rule for finder
+dates = worksheet.findall(
+    regex_numbers)  # Listing all dates in the worksheet using our rule
+regex_time = re.compile(
+    '[0-9][0-9].[0-9][0-9]-[0-9][0-9].[0-9][0-9]')  # Creating a second rule
+# for finder
+lessonTimeList = worksheet.findall(
+    regex_time)  # Listing all lesson times in the worksheet using our rule
+startDate = dates[0].value[:-5]  # First day number
+endDate = dates[-1].value[:-5]  # Last day number
+
+firstDay = dates[0].row  # Getting a first day row number
+secondDay = dates[1].row  # Getting a second day row number
+thirdDay = dates[2].row  # Getting a third day row number
+fourthDay = dates[3].row  # Getting a fourth day row number
+fifthDay = dates[4].row  # Getting a fifth day row number
+sixthDay = dates[5].row  # Getting a sixth day row number
+
+""" First Day """
+row = firstDay
+firstDay_list = []  # List of info about lessons on Monday
+firstDay_lessons = []  # Lessons on first day
+firstDay_lessons.clear()
+while row < secondDay - 1:
+    row_value = worksheet.row_values(row)
+    if row_value[3] == "МБ3":
+        firstDay_lessons.append(row_value)
+        i = 0
+        t = 0
+        while i < len(firstDay_lessons):
+            del firstDay_lessons[i][0:2]
+            filtered = filter(lambda nonempty: nonempty != ''
+                              and nonempty != ' '
+                              and nonempty != 'МБ3'
+                              and nonempty != '1',
+                              firstDay_lessons[t])
+            t = t + 1
+            i = i + 1
+        row = row + 1
+        firstDay_list.append(list(filtered))
+    else:
+        row = row + 1
+
+# Checking if any lessons exist
+if len(firstDay_list) > 0:
+    firstDay_list.insert(0, dates[0].value)
+else:
+    firstDay_list.insert(0, "None")
+print(firstDay_list)
+
+""" Second Day """
+row = secondDay
+secondDay_list = []  # List of info about lessons on second day
+secondDay_lessons = []  # Lessons on second day
+secondDay_lessons.clear()
+while row < thirdDay - 1:
+    row_value = worksheet.row_values(row)
+    if row_value[3] == "МБ3" or row_value[3] == "1":
+        secondDay_lessons.append(row_value)
+        i = 0
+        t = 0
+        while i < len(secondDay_lessons):
+            del secondDay_lessons[i][0:2]
+            filtered = filter(lambda nonempty:
+                              nonempty != ''
+                              and nonempty != ' '
+                              and nonempty != 'МБ3'
+                              and nonempty != '1', secondDay_lessons[t])
+            t = t + 1
+            i = i + 1
+        row = row + 1
+        secondDay_list.append(list(filtered))
+    else:
+        row = row + 1
+
+# Checking if any lessons exist
+if len(secondDay_list) > 0:
+    secondDay_list.insert(0, dates[1].value)
+else:
+    secondDay_list.insert(0, "None")
+print(secondDay_list)
+
+""" Third Day """
+row = thirdDay
+thirdDay_list = []  # List of info about lessons on third day
+thirdDay_lessons = []  # Lessons on third day
+thirdDay_lessons.clear()
+while row < fourthDay - 1:
+    row_value = worksheet.row_values(row)
+    if row_value[3] == "МБ3" or row_value[3] == "1" \
+            or row_value[4] == "Физическая культура и спорт ":
+        thirdDay_lessons.append(row_value)
+        i = 0
+        t = 0
+        while i < len(thirdDay_lessons):
+            del thirdDay_lessons[i][0:2]
+            filtered = filter(lambda nonempty:
+                              nonempty != ''
+                              and nonempty != ' '
+                              and nonempty != 'МБ3'
+                              and nonempty != '1',
+                              thirdDay_lessons[t])
+            t = t + 1
+            i = i + 1
+        row = row + 1
+        thirdDay_list.append(list(filtered))
+    elif row_value[5] == "Луканина Екатерина Викторовна":
+        spanish_time = row_value[2]
+        row = row + 1
+    elif row_value[5] == "Воинова Ольга Зиновьевна":
+        thirdDay_lessons.append(row_value)
+        i = 0
+        t = 0
+        while i < len(thirdDay_lessons):
+            del thirdDay_lessons[i][0:2]
+            filtered = filter(lambda nonempty:
+                              nonempty != ''
+                              and nonempty != ' ', thirdDay_lessons[t])
+            thirdDay_lessons[t].insert(0, spanish_time)
+            t = t + 1
+            i = i + 1
+        row = row + 1
+        thirdDay_list.append(list(filtered))
+    else:
+        row = row + 1
+# Checking if any lessons exist
+if len(thirdDay_list) > 0:
+    thirdDay_list.insert(0, dates[2].value)
+else:
+    thirdDay_list.insert(0, "None")
+print(thirdDay_list)
+
+""" Fourth Day """
+row = fourthDay
+fourthDay_list = []  # List of info about lessons on fourth day
+fourthDay_lessons = []  # Lessons on fourth day
+fourthDay_lessons.clear()
+while row < fifthDay - 1:
+    row_value = worksheet.row_values(row)
+    if row_value[3] == "МБ3" or row_value[3] == "1":
+        fourthDay_lessons.append(row_value)
+        i = 0
+        t = 0
+        while i < len(fourthDay_lessons):
+            del fourthDay_lessons[i][0:2]
+            filtered = filter(lambda nonempty:
+                              nonempty != ''
+                              and nonempty != ' '
+                              and nonempty != 'МБ3'
+                              and nonempty != '1', fourthDay_lessons[t])
+            t = t + 1
+            i = i + 1
+        row = row + 1
+        fourthDay_list.append(list(filtered))
+    else:
+        row = row + 1
+
+# Checking if any lessons exist
+if len(fourthDay_list) > 0:
+    fourthDay_list.insert(0, dates[3].value)
+else:
+    fourthDay_list.insert(0, "None")
+print(fourthDay_list)
+
+""" Fifth Day """
+row = fifthDay
+fifthDay_list = []  # List of info about lessons on fifth day
+fifthDay_lessons = []  # Lessons on fifth day
+fifthDay_lessons.clear()
+while row < sixthDay - 1:
+    row_value = worksheet.row_values(row)
+    if row_value[3] == "МБ3" or row_value[3] == "1" \
+            or row_value[4] == "Физическая культура и спорт ":
+        fifthDay_lessons.append(row_value)
+        i = 0
+        t = 0
+        while i < len(fifthDay_lessons):
+            del fifthDay_lessons[i][0:2]
+            filtered = filter(lambda nonempty:
+                              nonempty != ''
+                              and nonempty != ' '
+                              and nonempty != 'МБ3'
+                              and nonempty != '1', fifthDay_lessons[t])
+            t = t + 1
+            i = i + 1
+        row = row + 1
+        fifthDay_list.append(list(filtered))
+    elif row_value[5] == "Луканина Екатерина Викторовна":
+        spanish_time = row_value[2]
+        row = row + 1
+    elif row_value[5] == "Воинова Ольга Зиновьевна":
+        fifthDay_lessons.append(row_value)
+        i = 0
+        t = 0
+        while i < len(fifthDay_lessons):
+            del fifthDay_lessons[i][0:2]
+            filtered = filter(lambda nonempty:
+                              nonempty != '' and
+                              nonempty != ' ', fifthDay_lessons[t])
+            fifthDay_lessons[t].insert(0, spanish_time)
+            t = t + 1
+            i = i + 1
+        row = row + 1
+        fifthDay_list.append(list(filtered))
+    else:
+        row = row + 1
+
+# Checking if any lessons exist
+if len(fifthDay_list) > 0:
+    fifthDay_list.insert(0, dates[4].value)
+else:
+    fifthDay_list.insert(0, "None")
+print(fifthDay_list)
+
+""" Sixth Day """
+row = sixthDay
+sixthDay_list = []  # List of info about lessons on sixth day
+sixthDay_lessons = []  # Lessons on sixth day
+sixthDay_lessons.clear()
+while row <= lessonTimeList[-1].row:
+    row_value = worksheet.row_values(row)
+    if row_value[3] == "МБ3" or row_value[3] == "1" \
+            or row_value[4] == "Физическая культура и спорт ":
+        sixthDay_lessons.append(row_value)
+        i = 0
+        t = 0
+        while i < len(sixthDay_lessons):
+            del sixthDay_lessons[i][0:2]
+            filtered = filter(lambda nonempty:
+                              nonempty != '' and nonempty != ' '
+                              and nonempty != 'МБ3'
+                              and nonempty != '1', sixthDay_lessons[t])
+            t = t + 1
+            i = i + 1
+        row = row + 1
+        sixthDay_list.append(list(filtered))
+    else:
+        row = row + 1
+
+# Checking if any lessons exist
+if len(sixthDay_list) > 0:
+    sixthDay_list.insert(0, dates[5].value)
+else:
+    sixthDay_list.insert(0, "None")
+print(sixthDay_list)
+
+""" Pickle """
+data = [firstDay_list, secondDay_list, thirdDay_list, fourthDay_list,
+        fifthDay_list, sixthDay_list]
+
+with open('data.pkl', 'wb') as f:
+    pickle.dump(data, f)
